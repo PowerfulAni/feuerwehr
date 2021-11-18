3f722c461254690c20589f638f1e79e11fbe59b9
Schlump02 <94486540+Schlump02@users.noreply.github.com>
2021-11-18 14:21:44 +0100

    Feuerwehrmensch Konstruktor, get & set Methoden

--- FeuerwehrProject/src/menschen/Feuerwehrmensch.java
+++ FeuerwehrProject/src/menschen/Feuerwehrmensch.java
@@ -1,5 +1,35 @@
 package menschen;
 
-public class Feuerwehrmensch {
+import util.FahrzeugTyp;
+import util.MitarbeiterStatus;
 
+public class Feuerwehrmensch {
+	
+	private MitarbeiterStatus status;
+	private String name;
+	private FahrzeugTyp fahrerlaubnis;
+	
+	public Feuerwehrmensch(MitarbeiterStatus status, String name, FahrzeugTyp fahrerlaubnis) {
+		
+		this.status = status;
+		this.name = name;
+		this.fahrerlaubnis = fahrerlaubnis;
+	}
+	
+	
+	public MitarbeiterStatus getStatus() {
+		return status;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public FahrzeugTyp getFahrerlaubnis() {
+		return fahrerlaubnis;
+	}
+	
+	public void setMitarbeiterStatus(MitarbeiterStatus status) {
+		this.status = status;
+	}
 }
