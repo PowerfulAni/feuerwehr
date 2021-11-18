79b5835f2415a200c83f6d56b2af1ee01380115b
Schlump02 <94486540+Schlump02@users.noreply.github.com>
2021-11-18 14:09:08 +0100

    Enums erstellt.

---
+++ FeuerwehrProject/src/util/FahrzeugStatus.java
@@ -0,0 +1,6 @@
+package util;
+
+public enum FahrzeugStatus {
+	
+	Beraeit, Einsatz, Wartung;
+}
---
+++ FeuerwehrProject/src/util/MitarbeiterStatus.java
@@ -0,0 +1,6 @@
+package util;
+
+public enum MitarbeiterStatus {
+	
+	Bereit, Einsatz, Krank, Urlaub;
+}
