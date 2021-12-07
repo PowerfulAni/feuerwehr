e65f0d07b71df51e5769d1396a44c6ca94c0813a
Marius Mauer <marius@mauer.dev>
2021-12-07 09:55:24 +0100

    Update

--- FeuerwehrProject/src/gui/FahrzeugView.java
+++ FeuerwehrProject/src/gui/FahrzeugView.java
@@ -23,7 +23,7 @@ public class FahrzeugView extends JScrollPane {
 		ArrayList fahrzeuge = feuerwache.getFahrzeuge();
 		String content = "<html><center><table style='font-size: 12px; margin: 10px; width: 90%;'><tr style='font-size: 15px; padding: 5px; margin: 5px;'><th>Kennzeichen</th><th>Fahrzeugtyp</th><th>Sitzplätze</th><th>Status</th></tr>";
 		for (int i = 0; i < fahrzeuge.size(); i++) {
-			content += "<tr style='border-bottom: 1px dotted black; text-align: center;'><td style='border: 1px solid black'>" + ((Fahrzeug) fahrzeuge.get(i)).getKennzeichenString() + "</td><td>"
+			content += "<tr style='border-bottom: 1px dotted black; text-align: center;'><td style='border: 1px solid black'>" + ((Fahrzeug) fahrzeuge.get(i)).getKennzeichen() + "</td><td>"
 					+ ((Fahrzeug) fahrzeuge.get(i)).getFahrzeugTyp() + "</td><td>"
 					+ ((Fahrzeug) fahrzeuge.get(i)).getSitzplaetze() + "</td><td>"
 					+ ((Fahrzeug) fahrzeuge.get(i)).getFahrzeugStatus() + "</td><td>"
